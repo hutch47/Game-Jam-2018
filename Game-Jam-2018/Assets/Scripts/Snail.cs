da86ab4c88caf125b272e2a168db89e5a15d1207
Conrad Belaire <cbelaire@gmail.com>
2018-01-27 16:44:03 -0500

    Many changes
    Including egg separation, tile changes, ConradScene changes, and Snail Script

---
+++ Game-Jam-2018/Assets/Scripts/Snail.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snail : MonoBehaviour {
+    //Just gonna go ahead and steal some code from Jacob's TiScript
+
+    //I guess it just knows where it is
+    public Vector3 pos;
+    public Rigidbody2D rb; //and again, whatever you want to call it
+
+    public bool falling = false;
+    private bool jumping = false;
+
+    public int health = 30;
+
+    public float relScaleX;
+    public float relScaleY;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+        Debug.Log("pos is " + pos.x);
+        rb = GetComponent<Rigidbody2D>();
+        relScaleX = transform.localScale.x;
+        relScaleY = transform.localScale.y;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 position = this.transform.position;
+        position.x -= 0.03f;
+        this.transform.position = position;
+        transform.localScale = new Vector3(relScaleX, relScaleY, 1);
+    }
+}
