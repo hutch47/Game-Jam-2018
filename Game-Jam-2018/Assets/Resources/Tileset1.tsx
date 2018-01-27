25faa6f3dc553eeb40d1924e51238933d34fad1a
ConradBelaire <cbelaire@gmail.com>
2018-01-27 13:28:57 -0500

    Added Tiled2Unity + Test map
    Now can import map files! There's a test in Assets/Tiled2Unity/Prefabs, with collision.

---
+++ Game-Jam-2018/Assets/Resources/Tileset1.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset1" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="tileset1.png" width="512" height="512"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
