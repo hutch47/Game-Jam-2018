f3eadec73935c3cad3f1895058bc1954091b9051
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-01-27 15:32:43 -0500

    Jacob: Added support for different relative scales for wall jumping and regular jumping woo... I also added some stuff to my scene to test with... Also wall jumping

--- Game-Jam-2018/Assets/Scripts/TiGroundCollier.cs
+++ Game-Jam-2018/Assets/Scripts/TiGroundCollier.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class TiGroundCollier : MonoBehaviour {
 
     TiScript script;
+    float relScale;
 
 	// Use this for initialization
 	void Start () {
         script = this.transform.parent.GetComponent<TiScript>();
+        relScale = script.relScaleX;
     }
 
     // Update is called once per frame
     void Update() {
         //Add layer mask later
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position - new Vector3(0, 0.65f), 0.3f);
-        print("Num colliders" + hitColliders.Length);
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position - new Vector3(0, 0.80f / relScale, 0), new Vector2(0.5f / relScale, 1.7f / relScale), Vector2.Angle(Vector2.zero, transform.position));
         if (hitColliders.Length > 1)
         {
             script.falling = false;
         }
-        else
+
+        hitColliders = Physics2D.OverlapCircleAll(transform.position - new Vector3(0.9f / relScale, 0, 0), 0.2f / relScale);
+        if (hitColliders.Length > 1)
         {
-            script.falling = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                script.rb.AddForce(new Vector2(2f / relScale, 3f / relScale), ForceMode2D.Impulse);
+            }
+        }
+
+        hitColliders = Physics2D.OverlapCircleAll(transform.position + new Vector3(0.9f / relScale, 0, 0), 0.2f / relScale);
+        if (hitColliders.Length > 1)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                script.rb.AddForce(new Vector2(-2f / relScale, 3f / relScale), ForceMode2D.Impulse);
+            }
         }
     }
 
@@ -30,6 +45,8 @@ public class TiGroundCollier : MonoBehaviour {
     {
         Vector3 centre = transform.position;
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(centre - new Vector3(0, 0.65f, 0), new Vector3(1.5f, 0.4f));
+        Gizmos.DrawWireCube(centre - new Vector3(0, 0.80f / relScale, 0), new Vector3(1.75f / relScale, 0.5f / relScale));
+        Gizmos.DrawWireSphere(centre - new Vector3(0.27f / relScale, 0, 0), 0.7f / relScale);
+        Gizmos.DrawWireSphere(centre + new Vector3(0.27f / relScale, 0, 0), 0.7f / relScale);
     }
 }
--- Game-Jam-2018/Assets/Scripts/TiScript.cs
+++ Game-Jam-2018/Assets/Scripts/TiScript.cs
@@ -12,11 +12,16 @@ public class TiScript : MonoBehaviour {
 
     public int health = 30;
 
+    public float relScaleX;
+    public float relScaleY;
+
     // Use this for initialization
     void Start () {
         pos = transform.position;
         Debug.Log("pos is " + pos.x);
         rb = GetComponent<Rigidbody2D>();
+        relScaleX = transform.localScale.x;
+        relScaleY = transform.localScale.y;
     }
 
     // Update is called once per frame
@@ -28,12 +33,14 @@ public class TiScript : MonoBehaviour {
             Vector3 position = this.transform.position;
             position.x += 0.05f;
             this.transform.position = position;
+            transform.localScale = new Vector3(-relScaleX, relScaleY, 1);
         }
         else if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
         {
             Vector3 position = this.transform.position;
             position.x -= 0.05f;
             this.transform.position = position;
+            transform.localScale = new Vector3(relScaleX, relScaleY, 1);
         }
 
         if (Input.GetButton("Jump"))
