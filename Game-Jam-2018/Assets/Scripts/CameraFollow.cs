227439be4687c0a85c43aad5cc5fbd260e7d85b9
matteofilia <matteo@matteofilia.com>
2018-01-28 12:49:49 -0500

    Fixed camera follow script

--- Game-Jam-2018/Assets/Scripts/CameraFollow.cs
+++ Game-Jam-2018/Assets/Scripts/CameraFollow.cs
@@ -7,38 +7,38 @@ public class CameraFollow : MonoBehaviour {
 	public GameObject target;
 	public float offset = 2.5f;
 
-	private SpriteRenderer sr;
-
 	private bool flip_state_last;
 	private float animation_time = 0f;
 	public float transition_speed = 0.5f;
 
 	void Start() {
-		sr = target.GetComponentInChildren<SpriteRenderer>();
-		flip_state_last = sr.flipX;
+		flip_state_last = target.transform.localScale.x < 0;
 
-		transform.position = target.transform.position;
+
+		transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
 	}
 
 	void LateUpdate() {
 		Vector3 tp = target.transform.position; // Target position
 		Vector3 cp = transform.position; // Camera position
 
-		if (sr.flipX != flip_state_last) {
+		bool flipped = target.transform.localScale.x < 0;
+
+		if (flipped != flip_state_last) {
 			animation_time = 0f;
-			flip_state_last = sr.flipX;
+			flip_state_last = flipped;
 		}
 
 		if (animation_time < transition_speed) {
 			animation_time += Time.deltaTime;
 
-			if (sr.flipX) {
+			if (flipped) {
 				cp.x = tp.x + offset - (2 * offset * (animation_time / transition_speed));
 			} else {
 				cp.x = tp.x - offset + (2 * offset * (animation_time / transition_speed));;
 			}
 		} else {
-			if (sr.flipX) {
+			if (flipped) {
 				cp.x = tp.x - offset;
 			} else {
 				cp.x = tp.x + offset;
